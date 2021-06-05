424cbd767cfd244d4919b51c91c7828fa4de0238
Salo1802 <salo1802@hotmail.com>
2021-06-04 21:11:12 -0500

    added polo atributes

---
+++ src/Polo.java
@@ -0,0 +1,15 @@
+
+public class Polo extends Thread{
+public int posX;
+public int posY;
+private boolean right;
+private boolean up;
+private int mPosX;
+private int mPosY;
+	
+@Override
+public void run() {
+	
+	
+}
+}
